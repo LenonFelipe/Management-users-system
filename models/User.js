86d769d07269aa7ba1d19e3f67639b40b04533dc
LenonFelipe <lenonfelipe98@gmail.com>
2025-04-15 15:16:59 -0300

    feat: add user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    resetToken: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    resetTokenExpires: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    }
+});
+
+module.exports = User;
