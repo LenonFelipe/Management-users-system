d5941da2500f940d7c47e5226f60185214107074
LenonFelipe <lenonfelipe98@gmail.com>
2025-04-15 15:02:36 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const authRoutes = require('.routes/authRoutes');
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.listen(3000, () => {
+    console.log("Servidor aberto na porta 3000")
+});
