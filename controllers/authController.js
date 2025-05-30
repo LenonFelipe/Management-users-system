312e8a35ca41a50b507b40a02bda13e1e4a12a09
LenonFelipe <lenonfelipe98@gmail.com>
2025-05-30 00:58:50 -0300

    update: create a db user and controller for user register

---
+++ controllers/authController.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const register = async(req, res) => {
+    const {name, email, password} = req.body;
+
+    try {
+        const existingUser = await User.findOnde({ where: { email } });
+        if(existingUser) {
+            return res.status(400).json({ message: "E-mail já cadastrado"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await User.create({
+            nome,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({ message: 'Usuário criado com sucesso', user: {id: newUser.id, name: newUser.name, email: newUser.email }});
+    } catch (error) {
+        console.log("error no registro", error);
+        res.status(500).json({ message: "Erro ao registrar usuário" });
+    }
+};
+
+module.exports = {register};
