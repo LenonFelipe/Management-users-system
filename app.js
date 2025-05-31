9f62f70b737329ef0be66e1cd44f7bcfde35a039
LenonFelipe <lenonfelipe98@gmail.com>
2025-05-31 12:14:09 -0300

    fix: database connection and server is running

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const authRoutes = require('.routes/authRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
